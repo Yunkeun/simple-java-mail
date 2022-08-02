6fa7f9e8a6053456747a052e0dfd24f0fe04c512
Yunkeun <yunx2@naver.com>
2022-08-02 17:22:25 +0900

    feat: 스프링 프로젝트 초기 세팅

---
+++ simple-java-mail/src/main/java/com/yoonveloping/simplemail/SimpleMailApplication.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.simplemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMailApplication.class, args);
+	}
+
+}
---
+++ simple-java-mail/src/test/java/com/yoonveloping/simplemail/SimpleMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.simplemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
